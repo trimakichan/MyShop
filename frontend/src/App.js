005ea3d2c863500036f3dd4926cd63c9ce5b1e53
trimakichan <115846240+trimakichan@users.noreply.github.com>
2023-04-02 16:05:47 -0700

    frontend is done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import React from "react";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import {Container} from 'react-bootstrap';
-import HomeScreen from "./screens/HomeScreen";
-
+import React from 'react';
+import {Container} from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-
-<>
-<Header />
-<main>
-  <Container>
-<HomeScreen />
- </Container>
- </main>
- <Footer/>
-
-</>
-
- 
+    <>
+      <Router>
+        <Header />
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} exact />
+              <Route path="/product/:id" element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
