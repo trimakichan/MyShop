403d5e0742bf393c3b092cf4cd9c3dd55b7cee8d
trimakichan <115846240+trimakichan@users.noreply.github.com>
2023-04-02 21:54:51 -0700

    video 10

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Row from 'react-bootstrap/Row'
-import products from '../product'
 import Product from '../components/Product'
 import Col from 'react-bootstrap/Col'
+import {axios} from 'axios'
 
 const HomeScreen = () => {
+ useEffect(() => {
+  const fetchProducts = () => {
+    const {data} = axios.get('/api/products')
+  }
+ })
+
   return (
     <>
     <h1>Latest Products</h1>
