a9ceabd3afd638d20084c8a8bed2877b2c720a3f
trimakichan <115846240+trimakichan@users.noreply.github.com>
2023-05-26 14:14:27 -0700

    added PORT

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,9 @@ if (process.env.NODE_ENV === 'production'){
 
 app.use(errorHandler)
 
-app.listen(5000, console.log('server is running on port 5000'))
+const PORT = process.env.PORT 
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+
 
 
 
