97b80332bdd3d3586353c4f335fe417bffd05ddc
trimakichan <115846240+trimakichan@users.noreply.github.com>
2023-05-26 13:22:36 -0700

    test

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,18 @@ app.use(express.json())  //get the data in the form of a JSON from anywhere the 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.get('/api/config/paypal', (req, res) => 
+    res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
+//this runs our app in heroku
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build'))) 
+    app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })   
+};
 
 app.use(errorHandler)
 
